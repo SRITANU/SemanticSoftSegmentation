f09e6692458298b783898407d6dc142f874c8608
Sritanu Chakraborty <sritanu25@gmail.com>
2019-05-09 10:29:20 +0530

    Adds feature preprocessing to reduce dimensions

---
+++ feature_preprocess.py
@@ -0,0 +1,22 @@
+import scipy.io as sio
+import numpy as np
+from sklearn.decomposition import PCA
+
+mat = sio.loadmat('docia.mat')
+x = mat['embedmap']
+
+(h,w,d) = x.shape
+
+x = np.reshape(x, ((h*w), d))
+
+pca = PCA()
+
+y = pca.fit_transform(x)
+
+y = np.reshape(y, (h,w,3))
+
+for i in range(3):
+    y[:,:,i] = y[:,:,i] - y[:,:,i].min()
+    y[:,:,i] = y[:,:,i] - y[:,:,i].max()
+
+
